e1d00d49c33deae74c477556cb94a7c6435b046b
Owen Campbell <owen.campbell@tanti.org.uk>
2023-06-22 12:48:00 +0100

    Add setup for release version

--- electora_bot/bot.py
+++ electora_bot/bot.py
@@ -1,8 +1,10 @@
+import importlib.metadata
 import os
 
 import discord
 from discord.ext import commands
 
+__version__ = importlib.metadata.version("electora_bot")
 _prefix = os.getenv("COMMAND_PREFIX")
 _intents = discord.Intents.default()
 _intents.message_content = True
@@ -12,3 +14,8 @@ BOT = commands.Bot(command_prefix=_prefix, intents=_intents)
 @BOT.command()
 async def ping(ctx):
     await ctx.send("pong")
+
+
+@BOT.command()
+async def version(ctx):
+    await ctx.send(f"I'm Electora Bot version {__version__}")
