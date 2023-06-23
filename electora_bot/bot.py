eb05f505b13d94650b90534e9c0b5c05483bbb94
Owen Campbell <owen.campbell@tanti.org.uk>
2023-06-23 11:01:48 +0100

    Fix missing await

--- electora_bot/bot.py
+++ electora_bot/bot.py
@@ -10,7 +10,9 @@ COGS = (AdminCog, GreeterCog)
 
 
 class ElectoraBot(commands.Bot):
-    def __init__(self, prefix: str, intents: discord.Intents, guild_id: int = None) -> None:
+    def __init__(
+        self, prefix: str, intents: discord.Intents, guild_id: int = None
+    ) -> None:
         super().__init__(command_prefix=prefix, intents=intents)
         self.version = importlib.metadata.version("electora_bot")
         self.guild = discord.Object(id=guild_id) if guild_id else None
@@ -22,5 +24,5 @@ class ElectoraBot(commands.Bot):
 
     async def setup_hook(self) -> None:
         for cog in COGS:
-            await(self.add_cog(cog(self)))
-        self.sync()
+            await (self.add_cog(cog(self)))
+        await self.sync()
